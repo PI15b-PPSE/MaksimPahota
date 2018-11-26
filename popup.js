e3e3ced8b2d2e107eb1f9011ce1b735c76a09489
ImperiumB <pahmaks@gmail.com>
2018-11-26 18:06:30 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,17 +1,49 @@
-function generate(len)
-{
-   var ints =[0,1,2,3,4,5,6,7,8,9]; 
-   var chars=['a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z'];
-    var out='';
-    for(var i=0;i<len;i++){
-        var ch=Math.random(1,2);
-        if(ch<0.5){
-           var ch2=Math.ceil(Math.random(1,ints.length)*10);
-           out+=ints[ch2];
-        }else{
-           var ch2=Math.ceil(Math.random(1,chars.length)*10);
-           out+=chars[ch2];            
-        }
+//generate random password
+function generate(){
+
+    //set password length/complexity
+    let complexity = document.getElementById("slider").value;
+
+    //possible password values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    let password = "";
+
+    //create for loop to choose password characters
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
-    return out;
+
+    //add password to textbox/display area
+    document.getElementById("display").value = password;
+
+    //add password to previously generated passwords section
+    document.getElementById("lastNums").innerHTML += password + "<br />";
+
+}
+
+//set default length display of 25
+document.getElementById("length").innerHTML = "Length: 25";
+
+//function to set length based on slider position
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+
+}
+
+//function to copy password to clipboard
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to clipboard!");
+
 }
